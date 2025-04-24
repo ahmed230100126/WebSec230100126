f2dd63701ca30b2ceb160fef9ee08c803670847c
ahmed230100126 <ahmed230100126@sut.edu.eg>
2025-04-24 13:46:23 +0200

    "Modify some functions in the login process."

--- WebSecService./resources/views/multable.blade.php
+++ WebSecService./resources/views/multable.blade.php
@@ -1,14 +1,33 @@
 @extends('layouts.master')
 @section('title', 'Prime Numbers')
 @section('content')
-<div class="card m-4 col-sm-3">	
-  <div class="card-header">Multiplication Table of {{$j}}</div>
-  <div class="card-body">
-    <table>
-      @foreach (range(1, 10) as $i)
-      <tr><td>{{$i}} * {{$j}}</td><td> = {{ $i * $j }}</td></li>    
-      @endforeach
-    </table>
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-6 col-lg-4">
+      <div class="card shadow-sm my-4">
+        <div class="card-header bg-primary text-white">
+          <h5 class="mb-0">Multiplication Table of {{$j}}</h5>
+        </div>
+        <div class="card-body">
+          <table class="table table-striped table-bordered">
+            <thead class="bg-light">
+              <tr>
+                <th>Operation</th>
+                <th>Result</th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach (range(1, 10) as $i)
+              <tr>
+                <td class="fw-medium">{{$i}} × {{$j}}</td>
+                <td class="text-primary">{{ $i * $j }}</td>
+              </tr>
+              @endforeach
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 @endsection
--- WebSecService./resources/views/users/login.blade.php
+++ WebSecService./resources/views/users/login.blade.php
@@ -3,29 +3,33 @@
 @section('title', 'Login')
 
 @section('content')
-<div class="container">
+<div class="container py-5">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Login</div>
-                <div class="card-body">
+        <div class="col-md-6">
+            <div class="card shadow">
+                <div class="card-header bg-primary text-white">
+                    <h4 class="mb-0">Login</h4>
+                </div>
+                <div class="card-body p-4">
                     @if (session('error'))
-                        <div class="alert alert-danger">
+                        <div class="alert alert-danger alert-dismissible fade show">
                             {{ session('error') }}
+                            <button type="button" class="close" data-dismiss="alert">&times;</button>
                         </div>
                     @endif
                     
                     @if (session('success'))
-                        <div class="alert alert-success">
+                        <div class="alert alert-success alert-dismissible fade show">
                             {{ session('success') }}
+                            <button type="button" class="close" data-dismiss="alert">&times;</button>
                         </div>
                     @endif
 
-                    <form method="POST" action="{{ route('do_login') }}">
+                    <form method="POST" action="{{ route('do_login') }}" class="mb-4">
                         @csrf
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">Email Address</label>
-                            <div class="col-md-6">
+                        <div class="form-group row mb-3">
+                            <label for="email" class="col-md-4 col-form-label">Email Address</label>
+                            <div class="col-md-8">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email', session('email')) }}" required autocomplete="email" autofocus>
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -35,9 +39,9 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
-                            <div class="col-md-6">
+                        <div class="form-group row mb-4">
+                            <label for="password" class="col-md-4 col-form-label">Password</label>
+                            <div class="col-md-8">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -47,34 +51,33 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
+                        <div class="form-group row">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary btn-block">
                                     Login
                                 </button>
                                 
-                                <a href="{{ route('password.request') }}" class="btn btn-link">
+                                <a href="{{ route('password.request') }}" class="btn btn-link px-0">
                                     Forgot Your Password?
                                 </a>
                             </div>
                         </div>
                     </form>
                     
-                    <div class="row mt-3">
-                        <div class="col-md-8 offset-md-4">
-                            <a href="{{ route('redirectToFacebook') }}" class="btn btn-primary">
-                                <i class="fab fa-facebook-f mr-2"></i> Login with Facebook
-                                
+                    <div class="text-center mt-4">
+                        <p class="text-muted mb-3">Or login with</p>
+                        <div class="d-flex justify-content-center gap-2">
+                            <a href="{{ route('redirectToFacebook') }}" class="btn btn-primary mx-1">
+                                <i class="fab fa-facebook-f"></i> Facebook
                             </a>
-                            <a href="{{route('login_with_google')}}" class="btn btn-success">Login with
-                                Google</a>
-
-                                <a href="{{ route('login.github') }}" class="btn btn-dark btn-block">
-                                    <i class="fab fa-github"></i> Login with GitHub
-                                </a>    
+                            <a href="{{ route('login_with_google') }}" class="btn btn-danger mx-1">
+                                <i class="fab fa-google"></i> Google
+                            </a>
+                            <a href="{{ route('login.github') }}" class="btn btn-dark mx-1">
+                                <i class="fab fa-github"></i> GitHub
+                            </a>
                         </div>
                     </div>
-                  
                 </div>
             </div>
         </div>
