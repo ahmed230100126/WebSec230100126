14fe08e2f75410496b71906ef2515d022ec855fc
ahmed230100126 <ahmed230100126@sut.edu.eg>
2025-04-24 14:02:29 +0200

    Update the users display page and modify the reset function.

--- WebSecService./resources/views/users/list.blade.php
+++ WebSecService./resources/views/users/list.blade.php
@@ -1,63 +1,92 @@
 @extends('layouts.master')
 @section('title', 'Users')
 @section('content')
-<div class="row mt-2">
-    <div class="col col-10">
-        <h1>Users</h1>
+<div class="container py-4">
+  <div class="card shadow">
+    <div class="card-header bg-primary text-white d-flex justify-content-between align-items-center">
+      <h2 class="mb-0">Users</h2>
     </div>
-</div>
-<form>
-    <div class="row">
-        <div class="col col-sm-2">
-            <input name="keywords" type="text"  class="form-control" placeholder="Search Keywords" value="{{ request()->keywords }}" />
+    <div class="card-body">
+      @if(session('success'))
+        <div class="alert alert-success mb-3">
+          {{ session('success') }}
         </div>
-        <div class="col col-sm-1">
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
-        <div class="col col-sm-1">
-            <button type="reset" class="btn btn-danger">Reset</button>
-        </div>
-    </div>
-</form>
+      @endif
 
-<div class="card mt-2">
-  <div class="card-body">
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Name</th>
-          <th scope="col">Email</th>
-          <th scope="col">Roles</th>
-          <th scope="col"></th>
-        </tr>
-      </thead>
-      @foreach($users as $user)
-      <tr>
-        <td scope="col">{{$user->id}}</td>
-        <td scope="col">{{$user->name}}</td>
-        <td scope="col">{{$user->email}}</td>
-        <td scope="col">
-          @foreach($user->roles as $role)
-            <span class="badge bg-primary">{{$role->name}}</span>
+      @if(session('error'))
+        <div class="alert alert-danger mb-3">
+          {{ session('error') }}
+        </div>
+      @endif
+
+      <form class="mb-4">
+        <div class="row align-items-end">
+          <div class="col-md-4">
+            <div class="form-group">
+              <label for="keywords">Search</label>
+              <input id="keywords" name="keywords" type="text" class="form-control" placeholder="Search Keywords" value="{{ request()->keywords }}" />
+            </div>
+          </div>
+          <div class="col-auto">
+            <button type="submit" class="btn btn-primary">
+              <i class="bi bi-search"></i> Search
+            </button>
+          </div>
+          <div class="col-auto">
+            <a href="{{ route('users') }}" class="btn btn-outline-secondary">
+              <i class="bi bi-x-circle"></i> Reset 
+            </a>
+          </div>
+        </div>
+      </form>
+
+      <div class="table-responsive">
+        <table class="table table-striped table-hover">
+          <thead class="table-light">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Roles</th>
+              <th scope="col">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+          @foreach($users as $user)
+            <tr>
+              <td>{{$user->id}}</td>
+              <td>{{$user->name}}</td>
+              <td>{{$user->email}}</td>
+              <td>
+              @foreach($user->roles as $role)
+                <span class="badge bg-primary">{{$role->name}}</span>
+              @endforeach
+              </td>
+              <td>
+                <div class="btn-group" role="group">
+                @can('edit_users')
+                  <a class="btn btn-sm btn-primary" href='{{route('users_edit', [$user->id])}}'>
+                    <i class="bi bi-pencil"></i> Edit
+                  </a>
+                @endcan
+                @can('admin_users')
+                  <a class="btn btn-sm btn-info" href='{{route('edit_password', [$user->id])}}'>
+                    <i class="bi bi-key"></i> Password
+                  </a>
+                @endcan
+                @can('delete_users')
+                  <a class="btn btn-sm btn-danger" href='{{route('users_delete', [$user->id])}}'>
+                    <i class="bi bi-trash"></i> Delete
+                  </a>
+                @endcan
+                </div>
+              </td>
+            </tr>
           @endforeach
-        </td>
-        <td scope="col">
-          @can('edit_users')
-          <a class="btn btn-primary" href='{{route('users_edit', [$user->id])}}'>Edit</a>
-          @endcan
-          @can('admin_users')
-          <a class="btn btn-primary" href='{{route('edit_password', [$user->id])}}'>Change Password</a>
-          @endcan
-          @can('delete_users')
-          <a class="btn btn-danger" href='{{route('users_delete', [$user->id])}}'>Delete</a>
-          @endcan
-        </td>
-      </tr>
-      @endforeach
-    </table>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </div>
-
-
 @endsection
